33f5e3bb81aebe30d04ffb22d582d38e5afef4dc
Manoj Kumar Gurrala <manoj.lsi@gmail.com>
2022-06-13 13:28:52 +0530

    Update streamlit_app.py
    implemented header and text functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("My Parents new healthy Diner")
+streamlit.header("Breakfast Menu")
+streamlit.text("1. Dosa")
+streamlit.text("2. Idly")
+streamlit.text("3. Wada")
