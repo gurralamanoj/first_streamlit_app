679bec5a58883924aaeec3d281dbb3cfb0d9435f
Manoj Kumar Gurrala <manoj.lsi@gmail.com>
2022-06-13 15:56:50 +0530

    Update streamlit_app.py
    Spelling Corrected.

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some Fruits", list(my_fruit_list.index))
 # Pick list
-fruits_selected = streamlit.multiselect("Pick some Fruits", list(my_fruit_list.index),['Avacado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some Fruits", list(my_fruit_list.index),['Avocado','Strawberries'])
 #fruits_selected = streamlit.multiselect("Pick some Fruits", list(my_fruit_list.index))
 # Display the streamlit dataframe 
 # streamlit.dataframe(my_fruit_list)
