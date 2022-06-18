6a72098422514297a374197bf3ba2ac42ad7ff72
Manoj Kumar Gurrala <manoj.lsi@gmail.com>
2022-06-18 21:36:24 +0530

    Update streamlit_app.py
    imported snowflake.connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 streamlit.text(fruityvice_response)
 streamlit.text(fruityvice_response.json()) # Only writes data to Screen
 
+
+import snowflake.connector
 fruityvice_response = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_response)
 
