a9c4ff4f16e44ef5788fe9b72208e1c7db55b240
Manoj Kumar Gurrala <manoj.lsi@gmail.com>
2022-06-18 20:30:22 +0530

    Update streamlit_app.py
    implementing streamlit.text_input

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruity Vice Advice")
+fruit_choice = streamlit.text_input("Enter fruit name for info","Kiwi")
+streamlit.write ('Your Chocice : ' , fruit_choice)
+
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 streamlit.text(fruityvice_response)
