df305189a28bf48a5d4e2a913111fd0e08fbe2ec
Manoj Kumar Gurrala <manoj.lsi@gmail.com>
2022-10-24 19:32:22 +0530

    Update streamlit_app.py
    Badge 4 Exercise Commit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,6 +59,26 @@ streamlit.text("Hello from Snowflake")
 #streamlit.text("Data from Fruit load list")
 #streamlit.text(my_data_row)
 
+streamlit.text(" Application 2 Started for Badge 4 ")
+
+my_cur = my_cnx.cursor()
+# run a snowflake query and put it all in a var called my_catalog
+my_cur.execute("select color_or_style from catalog_for_website")
+my_catalog = my_cur.fetchall()
+# put the dafta into a dataframe
+df = pandas.DataFrame(my_catalog)
+# temp write the dataframe to the page so I Can see what I am working with
+# streamlit.write(df)
+# put the first column into a list
+color_list = df[0].values.tolist()
+# print(color_list)
+# Let's put a pick list here so they can pick the color
+option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
+# We'll build the image caption now, since we can
+product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
+# use the option selected to go back and get all the info from the database
+
+"""
 def get_Fruit_load_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("Select * from fruit_load_list")
@@ -80,4 +100,9 @@ if streamlit.button('Add fruit to list'):
     
 #streamlit.write ('Your Added : ' , add_my_fruit)
 #my_cur.execute("insert into fruit_data_list values ('"+ add_my_fruit +"')")
+"""
+streamlit.text(" End of Application ")
+
+
+
 
